2d0b4e1f7e9dec752da55c9b6596101482ec0fb3
jrichardlai <jrichardlai@gmail.com>
2012-11-23 15:39:51 -0800

    Ensure we can pass the protocol for replacement.

--- spec/lib/parser/text_spec.rb
+++ spec/lib/parser/text_spec.rb
@@ -1,56 +1,99 @@
 require 'spec_helper'
 
 describe Shrinker::Parser::Text do
-  let(:config) do
-    config = Shrinker::Config.new
-    config.instance_eval do
-      expanded_domain /(www\.)?google.com/
-      shrinked_domain 'goo.ln'
-    end
-    config
-  end
-
   describe "#replace" do
-    it "replaces proper occurences" do
 
-      content = <<-EV
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Nunc quis rutrum http://www.google.com?something=true&else=false dolor. 
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-        Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
-        Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
-        http://google.com/somepath?something=true
-        Aenean placerat ullamcorper lorem vel feugiat.
-      EV
+    context "without protocol" do
+      let(:config) do
+        config = Shrinker::Config.new
+        config.instance_eval do
+          expanded_domain /(www\.)?google.com/
+          shrinked_domain 'goo.ln'
+        end
+        config
+      end
 
-      Shrinker::Parser::Url.should_receive(:replace).with('www.google.com?something=true&else=false', {}, config).and_return("replace1")
-      Shrinker::Parser::Url.should_receive(:replace).with('google.fr?something=true', {}, config).never
-      Shrinker::Parser::Url.should_receive(:replace).with('google.com/somepath?something=true', {}, config).and_return("replace2")
+      it "replaces proper occurences" do
+        content = <<-EV
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc quis rutrum http://www.google.com?something=true&else=false dolor. 
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
+          Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
+          http://google.com/somepath?something=true
+          Aenean placerat ullamcorper lorem vel feugiat.
+        EV
 
-      replaced_text = Shrinker::Parser::Text::replace(content, {}, config)
+        Shrinker::Parser::Url.should_receive(:replace).with('www.google.com?something=true&else=false', {}, config).and_return("replace1")
+        Shrinker::Parser::Url.should_receive(:replace).with('google.fr?something=true', {}, config).never
+        Shrinker::Parser::Url.should_receive(:replace).with('google.com/somepath?something=true', {}, config).and_return("replace2")
 
-      expected_replaced_text = <<-EV
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Nunc quis rutrum http://replace1 dolor. 
-        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
-        Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
-        Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
-        http://replace2
-        Aenean placerat ullamcorper lorem vel feugiat.
-      EV
+        replaced_text = Shrinker::Parser::Text::replace(content, {}, config)
 
-      replaced_text.should == expected_replaced_text
+        expected_replaced_text = <<-EV
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc quis rutrum http://replace1 dolor. 
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
+          Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
+          http://replace2
+          Aenean placerat ullamcorper lorem vel feugiat.
+        EV
+
+        replaced_text.should == expected_replaced_text
+      end
+
+      it "does not do anything when there is no occurence" do
+        content = <<-EV
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
+          Aenean placerat ullamcorper lorem vel feugiat.
+        EV
+
+        replaced_text = Shrinker::Parser::Text::replace(content, {}, config)
+        replaced_text.should == content
+      end
     end
 
-    it "does not do anything when there is no occurence" do
-      content = <<-EV
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
-        Aenean placerat ullamcorper lorem vel feugiat.
-      EV
+    context "with protocol" do
+      let(:config) do
+        config = Shrinker::Config.new
+        config.instance_eval do
+          expanded_domain /http:\/\/(www\.)?google.com/
+          shrinked_domain 'https://goo.ln'
+        end
+        config
+      end
 
-      replaced_text = Shrinker::Parser::Text::replace(content, {}, config)
-      replaced_text.should == content
+      it "replaces proper occurences" do
+        content = <<-EV
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc quis rutrum http://www.google.com?something=true&else=false dolor. 
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
+          Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
+          https://google.com/somepath?something=true
+          Aenean placerat ullamcorper lorem vel feugiat.
+        EV
+
+        Shrinker::Parser::Url.should_receive(:replace).with('http://www.google.com?something=true&else=false', {}, config).and_return("https://replace1")
+        Shrinker::Parser::Url.should_receive(:replace).with('google.fr?something=true', {}, config).never
+        Shrinker::Parser::Url.should_receive(:replace).with('http://google.com/somepath?something=true', {}, config).never
+
+        replaced_text = Shrinker::Parser::Text::replace(content, {}, config)
+
+        expected_replaced_text = <<-EV
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nunc quis rutrum https://replace1 dolor. 
+          Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+          Curabitur ullamcorper nisl non dolor http://google.fr?something=true venenatis consequat.
+          Morbi odio libero, tincidunt quis tempus a, fringilla vitae augue.
+          https://google.com/somepath?something=true
+          Aenean placerat ullamcorper lorem vel feugiat.
+        EV
+
+        replaced_text.should == expected_replaced_text
+      end
     end
   end
 
--- spec/lib/parser/url_spec.rb
+++ spec/lib/parser/url_spec.rb
@@ -1,44 +1,68 @@
 require 'spec_helper'
 
 describe Shrinker::Parser::Url do
-  let(:config) do
-    config = Shrinker::Config.new
-    config.instance_eval do
-      backend         'Redis'
-      expanded_domain /(www\.)?google.com/
-      shrinked_domain 'goo.ln'
-    end
-    config
-  end
-
   describe "#replace" do
     before { Shrinker::Token.stub(:generate).and_return("thetoken") }
 
-    it "replace the link" do
-      Shrinker::Parser::Url::replace("www.google.com?something=else", {:user_id => 10}, config).should == "goo.ln/thetoken"
+    context "without protocol" do
+      let(:config) do
+        config = Shrinker::Config.new
+        config.instance_eval do
+          backend         'Redis'
+          expanded_domain /(www\.)?google.com/
+          shrinked_domain 'goo.ln'
+        end
+        config
+      end
+
+      it "replace the link" do
+        Shrinker::Parser::Url::replace("www.google.com?something=else", {:user_id => 10}, config).should == "goo.ln/thetoken"
+      end
+
+      it "store the old link" do
+        Shrinker::Backend::Redis.any_instance.should_receive(:store).with("www.google.com?something=else", 'thetoken', {:user_id => 10})
+
+        Shrinker::Parser::Url::replace("www.google.com?something=else", {:user_id => 10}, config)
+      end
+
+      describe "does not do anything" do
+        before do
+          Shrinker::Backend::Redis.any_instance.should_receive(:store).never
+        end
+
+        it "returns the text if it does not match an url" do
+          Shrinker::Parser::Url::replace("sometext", {}, config).should == "sometext"
+        end
+
+        it "returns the text if it does not match the domain" do
+          Shrinker::Parser::Url::replace("www.google.fr", {}, config).should == "www.google.fr"
+        end
+
+        it "does not replace the link if there is a shrinker=false" do
+          Shrinker::Parser::Url::replace("www.google.com?shrinker=false&something=else", {}, config).should == "www.google.com?something=else"
+        end
+      end
     end
 
-    it "store the old link" do
-      Shrinker::Backend::Redis.any_instance.should_receive(:store).with("www.google.com?something=else", 'thetoken', {:user_id => 10})
-
-      Shrinker::Parser::Url::replace("www.google.com?something=else", {:user_id => 10}, config)
-    end
-
-    describe "does not do anything" do
-      before do
-        Shrinker::Backend::Redis.any_instance.should_receive(:store).never
+    context "with protocol" do
+      let(:config) do
+        config = Shrinker::Config.new
+        config.instance_eval do
+          backend         'Redis'
+          expanded_domain /http:\/\/(www\.)?google.com/
+          shrinked_domain 'https://goo.ln'
+        end
+        config
       end
 
-      it "returns the text if it does not match an url" do
-        Shrinker::Parser::Url::replace("sometext", {}, config).should == "sometext"
+      it "replace the link" do
+        Shrinker::Parser::Url::replace("http://www.google.com?something=else", {:user_id => 10}, config).should == "https://goo.ln/thetoken"
       end
 
-      it "returns the text if it does not match the domain" do
-        Shrinker::Parser::Url::replace("www.google.fr", {}, config).should == "www.google.fr"
-      end
+      it "store the old link" do
+        Shrinker::Backend::Redis.any_instance.should_receive(:store).with("http://www.google.com?something=else", 'thetoken', {:user_id => 10})
 
-      it "does not replace the link if there is a shrinker=false" do
-        Shrinker::Parser::Url::replace("www.google.com?shrinker=false&something=else", {}, config).should == "www.google.com?something=else"
+        Shrinker::Parser::Url::replace("http://www.google.com?something=else", {:user_id => 10}, config)
       end
     end
   end
