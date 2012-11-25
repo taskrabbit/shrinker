09e813b9a7ba3efcaf3d897c3c420a6630be33e8
jrichardlai <jrichardlai@gmail.com>
2012-11-25 10:12:54 -0800

    Update version.rb

--- lib/shrinker/version.rb
+++ lib/shrinker/version.rb
@@ -1,3 +1,13 @@
 module Shrinker
-  VERSION = "0.0.1"
+  module VERSION
+
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 1
+    PRE   = nil
+
+    def self.to_s
+      [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+    end
+  end
 end
